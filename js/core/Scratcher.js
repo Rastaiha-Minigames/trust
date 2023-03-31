4071d1c9d26e06d7ff81e999313914eedbdfe918
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-04-01 03:15:31 +0330

    comment playing sound first

--- js/core/Scratcher.js
+++ js/core/Scratcher.js
@@ -63,7 +63,7 @@ Scratcher.scratchAnim = function(dom, scratchIn){
 	// setTimeout(function(){
 	// 	var sound = scratchIn ? Loader.sounds.scratch_in : Loader.sounds.scratch_out;
 	// 	sound.play();
-	// },1000);
+	// },100);
 
 	return deferred.promise;
 
