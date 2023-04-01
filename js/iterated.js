a530147f2c0a0ab27c56840f4fc74eb88940379b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-04-01 05:05:29 +0330

    add scratching then in all slides

--- js/iterated.js
+++ js/iterated.js
@@ -26,10 +26,9 @@ window.onload = function(){
 			function(ratio){
 				publish("preloader/progress", [ratio]);
 			}
-		);
-
-		Scratcher.scratch('iterated')
-
+		).then(()=>{
+			Scratcher.scratch('iterated');
+		});
 	});
 
 };
