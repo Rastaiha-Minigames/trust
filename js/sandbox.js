8d4c27f2db68329cac538105b42102c307d5114f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-04-01 06:15:44 +0330

    add sandbox

---
+++ js/sandbox.js
@@ -0,0 +1,34 @@
+var slideshow, slideSelect;
+window.onload = function(){
+
+	// PRELOADER
+	Q.all([
+		Loader.loadAssets(Loader.manifestPreload),
+		Words.convert("words.html")
+	]).then(function(){
+
+		// CHANGE DOM
+		document.body.removeChild($("#preloader"));
+		$("#main").style.display = "block";
+
+		// Slideshow
+		slideshow = new Slideshow({
+			dom: $("#slideshow"),
+			slides: SLIDES
+		});
+
+		// LOAD REAL THINGS
+		Loader.loadAssets(
+			Loader.manifest,
+			function(){
+				publish("preloader/done");
+			},
+			function(ratio){
+				publish("preloader/progress", [ratio]);
+			}
+		).then(()=>{
+			Scratcher.scratch('sandbox');
+		});
+	});
+
+};
